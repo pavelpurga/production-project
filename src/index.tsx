0e11bdb47121a65f1819fb341714a4a80673f515
pablo <pashapetrokas@gmail.com>
2025-12-19 13:33:53 +0300

    add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import App from './app/App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import 'shared/config/i18n/i18n';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
 );
 const element =
     <BrowserRouter>
-        <ThemeProvider>
-            <App/>
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App/>
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>
 root.render(element);
